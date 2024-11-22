aad54c9c1880d3b1de9bf8adf820bd5d2e8cbeda
Evgeniy Timoffev <geka.timof.max@gmail.com>
2024-11-22 22:49:45 +0000

    add right alphabet version

--- alphabet/main.py
+++ alphabet/main.py
@@ -2,89 +2,97 @@ import matplotlib
 matplotlib.use("TkAgg")
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage.measure import label, regionprops, perimeter
+from skimage.measure import label, regionprops, euler_number
 from collections import defaultdict
 from pathlib import Path
+from scipy.ndimage import binary_dilation
 
 
-def distance(v1, v2):
-    return ((v1 - v2) ** 2).sum() ** 0.5
+def recognize(region):
+    if region.image.mean() == 1.0:
+        return "-"
+    else:
+        image = region.image.copy()
+        struct = [[0,1,0],[1,1,1],[0,1,0]]
+        open_image = binary_dilation(image, struct)
+        enumber = euler_number(open_image)
+        if enumber == -1: # B or 8
+            have_vl = np.sum(np.mean(region.image[:, :region.image.shape[1] // 2],0) == 1) > 3
+            if have_vl:
+                return "B"
+            else:
+                return "8"
 
-def extractor(region):
-    area = np.sum(region.image) / region.image.size
-    perimeter_value = region.perimeter / region.image.size
-    # cy, cx = region.centroid_local
-    # cy /= region.image.shape[0]
-    # cx /= region.image.shape[1]
-    euler = region.euler_number
-    eccentricity = region.eccentricity
-    have_vl = np.sum(np.mean(region.image,0)) > 3
-    # new metrics
-    aspect_ratio = region.image.shape[1] / region.image.shape[0]
-    compactness = (4 * np.pi * region.area) / (region.perimeter or 1) ** 2
-    left_switch = sum(region.image[:,0][1:] != region.image[:,0][:-1])
-    # print(left_switch)
-    vertical_center_switch = sum(region.image[:,region.image.shape[1] // 2][1:] != region.image[:,region.image.shape[1] // 2][:-1])
-    # print(vertical_center_switch)
+        elif enumber == 0: # A, 0, P or D
+            have_vl = np.sum(np.mean(region.image[:, :region.image.shape[1] // 2], 0) == 1) > 3
+            if have_vl: # P, D
+                top_sum = region.image[:, :region.image.shape[1] * 2 // 3].sum() / (region.image.size * 2 // 3)
+                bot_sum = region.image[:, region.image.shape[1] * 2 // 3:].sum() / (region.image.size / 3)
+                # print(top_sum, bot_sum)
+                # print(np.isclose(top_sum, bot_sum, 0.2))
+                if np.isclose(top_sum, bot_sum, 0.2):
+                    return "D"
+                else:
+                    return "P"
 
-    return np.array([area, perimeter_value, euler, eccentricity, have_vl, aspect_ratio, compactness, left_switch, vertical_center_switch])
+            else:   # A, 0
+                cy, cx = region.centroid_local
+                cy /= region.image.shape[0]
+                cx /= region.image.shape[1]
+                # print(cy, cx)
+                if cy < 0.5:
+                    return "0"
+                else:
+                    return "A"
+        else: # /, W, X, * or 1
+            have_vl = np.sum(np.mean(region.image, 0) == 1) > 3
+            if have_vl:
+                return "1"
+            else:
+                if region.eccentricity < 0.45:
+                    return "*"
+                else:
+                    image = region.image.copy()
+                    image[0, :] = 1
+                    image[-1, :] = 1
+                    image[:, 0] = 1
+                    image[:, -1] = 1
+                    enumber = euler_number(image)
+                    if enumber == -1:
+                        return "/"
+                    elif enumber == -3:
+                        return "X"
+                    elif enumber == -4:
+                        return "W"
 
+    return "@"
 
-def classificator(region, classes):
-    det_class = None
-    v = extractor(region)
-    min_d = 10 ** 16
-    for cls in classes:
-        d = distance(v, classes[cls])
-        if d < min_d:
-            det_class = cls
-            min_d = d
-    return det_class
-
-template = plt.imread('files/alphabet-small.png')[:,:,:3].mean(2)
+# test img
+template = plt.imread('files/alphabet_ext.png')[:,:,:3].mean(2)
 template[template < 1] = 0
 template = np.logical_not(template)
-
 template_labeled = label(template)
-template_count = np.unique(template_labeled)[-1]
-print(f"template_count = {template_count}")
-
-regions = regionprops(template_labeled)
-classes = {"8": extractor(regions[0]),
-           "0": extractor(regions[1]),
-           "A": extractor(regions[2]),
-           "B": extractor(regions[3]),
-           "1": extractor(regions[4]),
-           "W": extractor(regions[5]),
-           "X": extractor(regions[6]),
-           "*": extractor(regions[7]),
-           "/": extractor(regions[8]),
-           "-": extractor(regions[9])
-           }
-
-alphabet = plt.imread('files/alphabet.png')[:,:,:3].mean(2)
+# real img
+alphabet = plt.imread('files/symbols.png')[:,:,:3].mean(2)
 alphabet[alphabet > 0] = 1
-
 alphabet_labeled = label(alphabet)
-alphabet_count = np.unique(alphabet_labeled)[-1]
-print(f"alphabet_count = {alphabet_count}")
 
-# show classes
-plt.figure()
-# for i, key  in enumerate(classes.keys()):
-#     plt.subplot(2,5,i+1)
-#     plt.title(f'{i} -> {key} ({classes[key]},)')
-#     plt.imshow(regions[i].image)
+regions = regionprops(alphabet_labeled)
 
-
+# result = defaultdict(lambda: 0)
+# for region in regions:
+#     symbol = recognize(region)
+#     result[symbol] += 1
+#
+# print(result)
 
 symbols = defaultdict(lambda: 0)
 path = Path("images")
 path.mkdir(exist_ok=True)
 plt.figure()
-for i, region in enumerate(regionprops(alphabet_labeled)):
+for i, region in enumerate(regions):
     print(region.label)
-    symbol = classificator(region, classes)
+    symbol = recognize(region)
     symbols[symbol] += 1
     plt.cla()
     plt.title(f"Symbol - {symbol}")
@@ -94,17 +102,7 @@ for i, region in enumerate(regionprops(alphabet_labeled)):
 print(symbols)
 
 
-# show raw data
 # plt.figure()
 # plt.subplot(211)
-# plt.imshow(template)
-#
-# plt.subplot(212)
-# plt.imshow(alphabet_labeled)
-plt.show()
-
-
-
-
-
-
+# plt.imshow(template_labeled)
+# plt.show()
